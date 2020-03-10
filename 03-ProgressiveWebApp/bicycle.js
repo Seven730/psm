314f743def086a6f9f54fb03afcc6898a1ba5088
Seven <michalwww3@gmail.com>
2020-03-10 12:49:31 +0100

    update

--- 03-ProgressiveWebApp/bicycle.js
+++ 03-ProgressiveWebApp/bicycle.js
@@ -1,8 +1,7 @@
-if ("sw" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-}
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js");
+  }
+};
